a6d636e8193a2083ce0167fed72b56317df20c68
istvano <istvan.orban@gmail.com>
2015-09-30 15:40:22 +0100

    refactoring install script to not include source-code in the image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 set -e
 
+appSetup () {
+	cd  ${ERPNEXT_SETUP_DIR}
+	./setup_frappe.sh --verbose --frappe-user=${ERPNEXT_USER}
+	service mysql stop
+	service nginx stop
+	service redis-server stop
+	service supervisor stop
+	update-rc.d -f mysql remove
+	update-rc.d -f nginx remove
+	update-rc.d -f redis-server remove
+	update-rc.d -f supervisor remove	
+	rm /etc/nginx/sites-enabled/default
+	
+	if [ ! -d "${ERPNEXT_INSTALL_DIR}" ]; then
+	  echo "Unable to set up Erp Next"
+	  exit 1
+	fi	
+	
+	# executed by --setup-production
+	cd ${ERPNEXT_INSTALL_DIR}
+	bench setup supervisor && bench setup nginx
+	ln -s ${ERPNEXT_INSTALL_DIR}/config/supervisor.conf /etc/supervisor/conf.d/frappe.conf
+	ln -s ${ERPNEXT_INSTALL_DIR}/config/nginx.conf /etc/nginx/conf.d/frappe.conf
+	cp ${ERPNEXT_SETUP_DIR}/config/all.conf /etc/supervisor/conf.d
+	
+	rm -rf ${ERPNEXT_INSTALL_DIR}/sites/site1.local/private/backups && ln -s ${ERPNEXT_BACKUP_DIR} ${ERPNEXT_INSTALL_DIR}/sites/site1.local/private/backups 
+}
+
+if [ ! -f "/etc/supervisor/conf.d/frappe.conf" ]; then
+  echo "Setting up ERP Next the first time"
+  appSetup
+fi
+	
 appStart () {
   # start supervisord
   exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
